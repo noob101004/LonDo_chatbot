4b6689ccea98c28958d417ebb6629cfbf4158114
noob101004 <u2444346@uel.ac.uk>
2025-04-27 15:05:26 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// API Keys and Configuration
+window.config = {
+    OPENWEATHER_API_KEY: 'YOUR_OPENWEATHER_API_KEY',  // This will be replaced by GitHub Actions
+    GOOGLE_AI_API_KEY: 'YOUR_GOOGLE_AI_API_KEY'      // This will be replaced by GitHub Actions
+};
+
+// Export the configuration
+module.exports = config; 
